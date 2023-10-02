919f910258cb87d1a2169b231ca195dce2c2d0ed
webdev-ashishk <webdev.ashishk@gmail.com>
2023-10-02 11:46:02 +0530

    side bar ready but add icons to sidebar items

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex">
       <div className="sidebar grid grid-cols-2 h-[84vh] bg-purple-300 place-items-baseline">
-        <ul className="grid gap-5 pl-5 pt-40">
+        <ul className="grid gap-5 pl-4 pt-40">
           <li>gmail</li>
           <li>github</li>
           <li>linkedin</li>
@@ -12,8 +12,23 @@ export default function Home() {
         </ul>
       </div>
       <div className="item border-2 border-red-500 grid w-full">
-      
-div      </div>
+        <div className="grid place-content-center h-[84vh]">
+          <h1 className="text-8xl font-bold">ASHISH KUMAR</h1>
+          <div className="flex justify-center">
+            <p class="text-2xl">
+              An experienced full-stack developer , tech instructor and writer.
+              <br></br> I enjoy building web-based applications that leaves a
+              lasting impression on users. <br></br>I contribute to Open Source
+              and write about my work on medium.
+            </p>
+          </div>
+          <div className="flex justify-center mt-8">
+            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-4 px-8 border-b-4 border-blue-700 hover:border-blue-500 rounded text-2xl">
+              Resume
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
